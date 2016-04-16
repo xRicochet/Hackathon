764359bb3e2e3a8f9c256cd69b2f49fd39658687
Daniel Moniry <daniel.moniry@gmail.com>
2016-04-16 17:22:18 +0300

    query change

--- Hackathon/Services/UserService.cs
+++ Hackathon/Services/UserService.cs
@@ -5,17 +5,18 @@ using System.Linq;
 using System.Web;
 using System.Configuration;
 using Dapper;
+using Hackathon.Data;
 
 namespace Hackathon.Services
 {
     public class UserService
     {
-        public static readonly string userQuery = "Select u.* from Users u where u.Id = ";
+        public static readonly string userQuery = "Select u.* from Users u";
         public User GetUser(int id)
         {
             using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ConnectionString))
             {
-                SqlCommand command = new SqlCommand(String.Concat(userQuery,id), connection);
+                SqlCommand command = new SqlCommand(String.Concat(userQuery,"where u.Id = ",id), connection);
 
 
                 connection.Open();
