4faef0c7e4ed5fe5dd3769a4f6764c61baae4bae
xRicochet <olteanu1995@yahoo.com>
2016-04-17 01:40:56 +0300

    xxxxxx

---
+++ Hackathon/Services/CommentariesService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using Dapper;
+using Hackathon.Data;
+
+namespace Hackathon.Services
+{
+    public class CommentariesService
+    {
+        public List<Commentaries> GetAllCommentariesAtAParty(int PartyId)
+    }
+}
