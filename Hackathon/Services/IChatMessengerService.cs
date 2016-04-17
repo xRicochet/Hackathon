0b5f08b3fb5d7c4553500a8fc9062bc8c908718d
xRicochet <olteanu1995@yahoo.com>
2016-04-17 06:18:02 +0300

    zzz

--- Hackathon/Services/IChatMessengerService.cs
+++ Hackathon/Services/IChatMessengerService.cs
@@ -11,7 +11,7 @@ namespace Hackathon.Services
 {
     public interface IChatMessengerService
     {
-        public List<String> GetChatMessages(int chatId);
-        public List<User> GetChatUsers(int chatId);
+         List<String> GetChatMessages(int chatId);
+         List<User> GetChatUsers(int chatId);
     }
 }
